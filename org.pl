a3459fdc6e8f54e1a07ec4fc049352e98574966f
Kevin Izevbigie <vinn@kevinize.com>
2023-11-02 01:28:22 +0700

    added staging folders and dl folders

--- org.pl
+++ org.pl
@@ -5,4 +5,19 @@ use File::Copy;
 
 my $user = $ENV{HOME};
 
-print $user;
+# Storage Directories
+my $image_staging = $user . '/Documents/staging/image-staging/';
+my $mp4_staging = $user . '/Documents/staging/mp4-staging/';
+my $mp3_staging = $user . '/Documents/staging/mp3-staging/';
+my $txt_staging = $user . '/Documents/staging/txt-staging/';
+my $pdf_staging = $user . '/Documents/staging/pdf-staging/';
+my $ebook_staging = $user . '/Documents/staging/ebook-staging/';
+my $csv_staging = $user . '/Documents/staging/csv-staging/';
+my $zip_staging = $user . '/Documents/staging/zip-staging/';
+
+# Merge path to full absolute path
+my $download_folder = $user . "Downloads" . "/";
+
+# Open directory
+opendir my $dir, $download_folder or die "Cannot open directory: $!";
+
